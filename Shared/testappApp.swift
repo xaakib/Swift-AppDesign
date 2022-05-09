6353f78077752999f37e5f064e7f38b5e7b9909a
Xaakib <64243974+xaakib@users.noreply.github.com>
2022-05-09 11:58:23 +0600

    Initial Commit

---
+++ Shared/testappApp.swift
@@ -0,0 +1,17 @@
+//
+//  testappApp.swift
+//  Shared
+//
+//  Created by Xaakib on 9/5/22.
+//
+
+import SwiftUI
+
+@main
+struct testappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
